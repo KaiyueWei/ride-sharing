ed400aa344f895454e7a7c247b5c13f180419793
KaiyueWei <kaiyue.wei@outlook.com>
2026-06-17 13:41:30 -0700

    add rabibtmq connection in trip service

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -9,8 +9,10 @@ import (
 	"ride-sharing/services/trip-service/internal/infrastructure/grpc"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"ride-sharing/shared/env"
 	"syscall"
 
+	amqp "github.com/rabbitmq/amqp091-go"
 	grpcserver "google.golang.org/grpc"
 )
 
@@ -24,7 +26,7 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	go func(){
+	go func() {
 		sigCh := make(chan os.Signal, 1)
 		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 		<-sigCh
@@ -36,19 +38,27 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	// RabbitMQ connection
+	conn, err := amqp.Dial(env.GetString("RABBITMQ_DEFAULT_URI", "amqp://guest:guest@rabbitmq:5672/"))
+	if err != nil {
+		log.Fatal("failed to connect to rabbitmq")
+	}
+	defer conn.Close()
+
+	// Starting the gRPC server
 	grpcServer := grpcserver.NewServer()
 	// todo initialize grpc handler implementation
 	grpc.NewGRPCHandler(grpcServer, svc)
 
 	log.Printf("starting gRPC server Trip Service on port %s", lis.Addr().String())
 
-	go func(){
+	go func() {
 		if err := grpcServer.Serve(lis); err != nil {
 			log.Printf("failed to serve: %v", err)
 			cancel()
 		}
 	}()
-	
+
 	// todo wait for the shutdown signal
 	<-ctx.Done()
 	log.Printf("Shutting down the server...")
